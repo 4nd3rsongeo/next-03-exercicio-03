906d23bddde687912b5fdac12fae0aa916fb5812
Anderson Miyashita <andsht@gmail.com>
2026-02-16 19:41:23 -0300

    atualizações em Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ export default function Home(){
     return (
         <main>
             <h1>Conheça as Naves do Universo de Star Wars</h1>
+            <h2>Atualização de página</h2>
+            <div className="tecnologias">
+                <h2>React</h2>
+                <h2>Next</h2>
+                <h2>Sass</h2>
+            </div>
 
             <p>
                 <Link className="btn" href={"/spaceships"}>Ver todas</Link>
